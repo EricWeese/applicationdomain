bd9913bb12473ed6390bdf9f318f77837a9b0958
JoshMeder <joshuameder@gmail.com>
2022-10-31 17:11:01 -0400

    CommentedOutUnusedCode...

--- my-app/src/pages/journals/Journal.js
+++ my-app/src/pages/journals/Journal.js
@@ -1,46 +1,43 @@
-import { useState } from "react";
-import { auth, db } from '../../firebase/config'
-
 import './Journal.css'
 
 export default function ManageUsers() {
-    const [username, getUserName] = useState('')
-    const [password, getPassword] = useState('')
+    // const [username, getUserName] = useState('')
+    // const [password, getPassword] = useState('')
 
-    const handleSubmit = async (e) => {
-        auth.signInWithEmailAndPassword(username, password)
-            .then(function () {
-                // Declare user variable
-                var user = auth.currentUser
+    // const handleSubmit = async (e) => {
+    //     auth.signInWithEmailAndPassword(username, password)
+    //         .then(function () {
+    //             // Declare user variable
+    //             var user = auth.currentUser
 
-                // Add this user to Firebase Database
-                var database_ref = database.ref()
+    //             // Add this user to Firebase Database
+    //             var database_ref = database.ref()
 
-                // Create User data
-                var user_data = {
-                    last_login: Date.now()
-                }
+    //             // Create User data
+    //             var user_data = {
+    //                 last_login: Date.now()
+    //             }
 
-                // Push to Firebase Database
-                database_ref.child('users/' + user.uid).update(user_data)
+    //             // Push to Firebase Database
+    //             database_ref.child('users/' + user.uid).update(user_data)
 
-                // Done
-                alert('User Logged In!!')
-                window.location = "./UserSplash.html";
+    //             // Done
+    //             alert('User Logged In!!')
+    //             window.location = "./UserSplash.html";
 
-            })
-            .catch(function (error) {
-                var loginAttempts = 3;
-                loginAttempts--;
+    //         })
+    //         .catch(function (error) {
+    //             var loginAttempts = 3;
+    //             loginAttempts--;
 
 
-                // Firebase will use this to alert of its errors
-                var error_code = error.code
-                var error_message = error.message
+    //             // Firebase will use this to alert of its errors
+    //             var error_code = error.code
+    //             var error_message = error.message
 
-                alert(error_message)
-            })
-    }
+    //             alert(error_message)
+    //         })
+    // }
 
     return (
         <div>
