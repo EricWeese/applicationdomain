760e05d806bba7089802726779b8792be7bf44c9
JoshMeder <joshuameder@gmail.com>
2022-11-02 22:54:00 -0400

    updatedNavbar

--- my-app/src/components/navbar/Navbar.js
+++ my-app/src/components/navbar/Navbar.js
@@ -21,7 +21,7 @@ export default function NavBar() {
         <div>
       <Navbar bg="primary" variant="dark" >
         <Container>
-          <Navbar.Brand href="/Home">Home</Navbar.Brand>
+          <Navbar.Brand href="/"> <img src="../assets/favicon.ico" alt="" width="30" height="24"/></Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link href="/Accounts">Accounts</Nav.Link>
             <Nav.Link href="/Users">Users</Nav.Link>
@@ -29,10 +29,10 @@ export default function NavBar() {
           </Nav>
         </Container>
         <Navbar.Text>
-            Signed in as: <a href="#login">Ez Books</a>
+            Signed in as: <a href="/AccountInfo">Ez Books</a>
         </Navbar.Text>
         <Form className="d-flex">
-            <Button variant="outline-success" onClick={logout}>Logout</Button>
+            <Button bg="primary" onClick={logout}>Logout</Button>
         </Form>
       </Navbar>
       <br />
