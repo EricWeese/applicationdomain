e89196a4a8e044954e837fb635213e08b9b1da2f
JoshMeder <joshuameder@gmail.com>
2022-11-03 13:15:21 -0400

    Revert "removingUnusedFiles"
    This reverts commit 388980106e853f1e5e38ac23db3b5726aa8c01eb.

---
+++ my-app/src/App.js
@@ -0,0 +1,9 @@
+import Signup from './pages/signup/Signup.js'
+import Login from './pages/login/Login.js'
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import { useContext } from "react";
+function App() {
+
+
+}
+export default App;
---
+++ my-app/src/components/TextField.js
@@ -0,0 +1,9 @@
+import './TextField.css'
+
+export default function TextField({title}){
+    return (
+        <div>
+            <button className="button">{title}</button>
+        </div>
+    )
+}
