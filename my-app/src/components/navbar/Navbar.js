25923db01d2832099caf3095c7bd71c3ececb747
EricWeese <ericweese1@gmail.com>
2022-11-05 12:49:38 -0400

    updated navbar

--- my-app/src/components/navbar/Navbar.js
+++ my-app/src/components/navbar/Navbar.js
@@ -8,20 +8,20 @@ import Button from 'react-bootstrap/Button';
 
 export default function NavBar() {
 
-    const logout = (e) =>{
-        const auth = getAuth();
-        signOut(auth).then(() => {
-        // Sign-out successful.
-        }).catch((e) => {
-        // An error happened.
-        });
-    }
+  const logout = (e) => {
+    const auth = getAuth();
+    signOut(auth).then(() => {
+      // Sign-out successful.
+    }).catch((e) => {
+      // An error happened.
+    });
+  }
 
-    return (
-        <div>
+  return (
+    <div>
       <Navbar bg="primary" variant="dark" >
         <Container>
-          <Navbar.Brand href="/"> <img src="../assets/favicon.ico" alt="" width="30" height="24"/></Navbar.Brand>
+          <Navbar.Brand href="/"> <img src="../../assets/favicon.ico" alt="" width="30" height="24" /></Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link href="/Accounts">Accounts</Nav.Link>
             <Nav.Link href="/Users">Users</Nav.Link>
@@ -29,13 +29,13 @@ export default function NavBar() {
           </Nav>
         </Container>
         <Navbar.Text>
-            Signed in as: <a href="/AccountInfo">Ez Books</a>
+          Signed in as: <a href="/AccountInfo">Ez Books</a>
         </Navbar.Text>
         <Form className="d-flex">
-            <Button bg="primary" onClick={logout}>Logout</Button>
+          <Button bg="primary" onClick={logout}>Logout</Button>
         </Form>
       </Navbar>
       <br />
-      </div>
-    )
+    </div>
+  )
 }
