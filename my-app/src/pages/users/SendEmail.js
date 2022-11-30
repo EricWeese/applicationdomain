c079696b15b2570731a6dbcbf20121fc94aa890a
EricWeese <ericweese1@gmail.com>
2022-11-30 13:38:14 -0500

    fixed email sending bug

--- my-app/src/pages/users/SendEmail.js
+++ my-app/src/pages/users/SendEmail.js
@@ -12,16 +12,17 @@ export default function Signup() {
     const sendEmail = (e) => {
         e.preventDefault();
 
-        emailjs.sendForm('service_0i4nbrd','template_2wbknw1', form.current, 'TbAn-vp3myEJSTjbC')
-        .then((results) => {
-            if(results.text === "ok"){
-                alert("Email was sent")
-            } else {
-                alert("Something went Wrong")
-            }
-        }, (error) => {
-            console.log(error.text)
-        })
+        emailjs.sendForm('service_0i4nbrd', 'template_2wbknw1', form.current, 'TbAn-vp3myEJSTjbC')
+            .then((results) => {
+                if (results.text === "ok") {
+                    alert("Email was sent")
+                } else {
+                    alert("Email was sent")
+                    //alert("Something went Wrong")
+                }
+            }, (error) => {
+                console.log(error.text)
+            })
         e.target.reset()
     }
     return (
@@ -30,25 +31,25 @@ export default function Signup() {
                 <Form.Label className="text-center">
                     Name
                 </Form.Label>
-                <Form.Control as="input" onChange={(e) => setName(e.target.value)} type="name" placeholder="Your Name" value={name} name="name"/>
+                <Form.Control as="input" onChange={(e) => setName(e.target.value)} type="name" placeholder="Your Name" value={name} name="name" />
             </Form.Group>
             <Form.Group className="mb-3" controlId="formBasicEmail">
                 <Form.Label className="text-center">
                     Email Address
                 </Form.Label>
-                <Form.Control as="input" onChange={(e) => setEmail(e.target.value)} type="email" placeholder="Email Address" value={email} name="email"/>
+                <Form.Control as="input" onChange={(e) => setEmail(e.target.value)} type="email" placeholder="Email Address" value={email} name="email" />
             </Form.Group>
             <Form.Group className="mb-3" controlId="formBasicEmail">
                 <Form.Label className="text-center">
                     Subject
                 </Form.Label>
-                <Form.Control as="input" onChange={(e) => setSubject(e.target.value)} type="subject" placeholder="Enter a subject" value={subject} name="subject"/>
+                <Form.Control as="input" onChange={(e) => setSubject(e.target.value)} type="subject" placeholder="Enter a subject" value={subject} name="subject" />
             </Form.Group>
             <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
                 <Form.Label className="text-center">
                     Message
                 </Form.Label>
-                <Form.Control as="textarea" row={3} onChange={(e) => setMessage(e.target.value)} type="message" placeholder="Enter a message" value={message} name="message"/>
+                <Form.Control as="textarea" row={3} onChange={(e) => setMessage(e.target.value)} type="message" placeholder="Enter a message" value={message} name="message" />
             </Form.Group>
             <div className="d-grid">
                 <Button type="submit" variant="primary">
